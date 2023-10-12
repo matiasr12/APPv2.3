be02fa75272cad3082be3d641d14a16239504e13
MatiasR <41710371+matiasr12@users.noreply.github.com>
2023-10-11 22:53:57 -0300

    version 2.4

--- app/src/main/java/com/example/app/Estadisticas.java
+++ app/src/main/java/com/example/app/Estadisticas.java
@@ -12,59 +12,115 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.app.Modelo.Usuarios;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Estadisticas extends Fragment {
+    Button btguardar;
+    EditText nTienda;
+    EditText nProducto;
+    EditText Kogramos;
+    EditText Precio;
+    EditText Dlocal;
+
 
     // funcion del regristro de datos de la tienda
     static final int REQUEST_IMAGEN_CAPTURE =1;
     private DatabaseReference mStorage;
-    private ListView ListaUsuarios;
-    ListView lvUsuarios;
-    ArrayList<Usuarios> Usuario;
-    ArrayAdapter<Usuarios> adpUsuario;
+    private FirebaseFirestore mfirestore;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mStorage = FirebaseDatabase.getInstance().getReference();
-
-
-
+        //mStorage = FirebaseDatabase.getInstance().getReference();
+        mfirestore = FirebaseFirestore.getInstance();
     }
-    public  void tomar_foto(View v)
-    {
-        Intent takePictureIntent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(takePictureIntent.resolveActivity(getActivity().getPackageManager())!=null){
-            startActivityForResult(takePictureIntent,REQUEST_IMAGEN_CAPTURE);
-        }
 
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_estadisticas, container, false);
-       // ListaUsuarios = (ListView) v.findViewById(R.id.lvUsuarios);
-        //ArrayList<Usuarios> usuarios = new ArrayList<Usuarios>();
-        //cargarBd();
+        nTienda = v.findViewById(R.id.nTienda);
+        nProducto = v.findViewById(R.id.nProducto);
+        Kogramos = v.findViewById(R.id.Kogramos);
+        Precio = v.findViewById(R.id.Precio);
+        Dlocal = v.findViewById(R.id.Dlocal);
+        btguardar = v.findViewById(R.id.btguardar);
+        btguardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombreTienda = nTienda.getText().toString().trim();
+                String nombreProductos = nProducto.getText().toString().trim();
+                String kilosOgramos = Kogramos.getText().toString().trim();
+                String precio = Precio.getText().toString().trim();
+                String dlocal = Dlocal.getText().toString().trim();
+
+                if(nombreTienda.isEmpty() && nombreProductos.isEmpty() && kilosOgramos.isEmpty() && precio.isEmpty() && dlocal.isEmpty()){
+                    Toast.makeText(requireContext(), "Debe completar los datos ", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    postTiendas(nombreTienda,nombreProductos,kilosOgramos,precio,dlocal);
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("nombreTienda",nombreTienda);
+                    map.put("nombreProductos",nombreProductos);
+                    map.put("kiloOgramos",kilosOgramos);
+                    map.put("percio",precio);
+                    map.put("direccionDelLocal",dlocal);
+
+                    mfirestore.collection("Productos").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Toast.makeText(requireContext(), "Creado exitosamente", Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(requireContext(), "Error al ingresar ", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+                }
+
+
+
+
+            }
+        });
+
+
+
+
+
+
         return v;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    private void postTiendas(String nombreTienda, String nombreProductos, String kilosOgramos, String precio, String dlocal) {
     }
+
+
 }
 
